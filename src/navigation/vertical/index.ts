c7f481117b182f4848ba0d1f375f3625a7dc273a
unknown <sean.jin@teehub1.com>
2023-05-18 11:56:29 -0700

    first commit

---
+++ src/navigation/vertical/index.ts
@@ -0,0 +1,103 @@
+// ** Icon imports
+import Logout from 'mdi-material-ui/Logout'
+import Table from 'mdi-material-ui/Table'
+import CubeOutline from 'mdi-material-ui/CubeOutline'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
+import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
+import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
+import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import WalletOutline from 'mdi-material-ui/WalletOutline'
+import MessageOutline from 'mdi-material-ui/MessageOutline'
+import ReportOutline from 'mdi-material-ui/ChartBar'
+import GeneralSetting from 'mdi-material-ui/CogOutline'
+
+
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Dashboard',
+      icon: HomeOutline,
+      path: '/'
+    },
+    {
+      title: 'Transaction',
+      icon: WalletOutline,
+      path: '/transaction'
+    },
+    {
+      title: 'Chatbot',
+      icon: MessageOutline,
+      path: '/chatbot'
+    },
+    {
+      title: 'Reports',
+      icon: ReportOutline,
+      path: '/reports'
+    },
+    {title: 'General Setting',
+      icon: GeneralSetting,
+    path:'/general-setting'},
+    {
+      sectionTitle: 'Accounts'
+    },
+    {
+      title: 'Account Settings',
+      icon: AccountCogOutline,
+      path: '/account-settings'
+    },
+    {
+      title: 'Logout',
+      icon: Logout,
+      path: '/pages/login',
+      openInNewTab: true
+    },
+    {
+      title: 'Register',
+      icon: AccountPlusOutline,
+      path: '/pages/register',
+      openInNewTab: true
+    },
+    {
+      title: 'Error',
+      icon: AlertCircleOutline,
+      path: '/pages/error',
+      openInNewTab: true
+    },
+    {
+      sectionTitle: 'User Interface'
+    },
+    {
+      title: 'Typography',
+      icon: FormatLetterCase,
+      path: '/typography'
+    },
+    {
+      title: 'Icons',
+      path: '/icons',
+      icon: GoogleCirclesExtended
+    },
+    {
+      title: 'Cards',
+      icon: CreditCardOutline,
+      path: '/cards'
+    },
+    {
+      title: 'Tables',
+      icon: Table,
+      path: '/tables'
+    },
+    {
+      icon: CubeOutline,
+      title: 'Form Layouts',
+      path: '/form-layouts'
+    }
+  ]
+}
+
+export default navigation
---
+++ src/pages/general-settings/index.tsx
@@ -0,0 +1,7 @@
+import React
+
+const AccountSettings = () => {
+    return (
+
+    )
+}
